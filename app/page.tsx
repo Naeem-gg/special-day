af8c0077484e0397b6fa883994e88d3e225205af
naeemgg <naeemqg1@gmail.com>
2024-10-29 22:57:44 +0530

    metadata and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next"
 import CoupleForm from "./components/CoupleForm"
 
-
+export const metaData:Metadata = {
+  title:"BIG DAY",
+  description:"A small project by NNavjivan to calculate the days left to your special day",
+  keywords:["big day","special day", "celebration day","count days from now"]
+}
 const page = () => {
   return (
     <div>
