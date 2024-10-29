e95e527ddd0d228641c447b322fb0be14600d460
naeemgg <naeemqg1@gmail.com>
2024-10-29 23:00:19 +0530

    metadata spelling

--- app/confetti/page.tsx
+++ app/confetti/page.tsx
@@ -1,7 +1,14 @@
+import { Metadata } from 'next'
 import Confetti from '../components/Confetti'
 import ShowData from './ShowData'
 
+export const metadata:Metadata = {
+  title:"BIG DAY",
+  description:"A small project by NNavjivan to calculate the days left to your special day",
+  keywords:["big day","special day", "celebration day","count days from now"]
+}
 const page = () => {
+  
   return (
     <div>
       <Confetti />
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next"
 import CoupleForm from "./components/CoupleForm"
 
-export const metaData:Metadata = {
+export const metadata:Metadata = {
   title:"BIG DAY",
   description:"A small project by NNavjivan to calculate the days left to your special day",
   keywords:["big day","special day", "celebration day","count days from now"]
